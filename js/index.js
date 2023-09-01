3b119b26be2d43258bc1a63dfb5af5a041c95ceb
Joao Bataglia <jpangottibataglia@gmail.com>
2023-09-01 00:34:20 -0300

    added background colors, more omnitrix styles for different occasions, improved omnitrix design, added more functions and removed bugs

--- js/index.js
+++ js/index.js
@@ -1,62 +1,74 @@
-import { playlistMatrix } from "./aliens.js"
+import { playlistMatrix, alienColorsMatrix } from "./aliens.js"
 
+const omnitrix = document.querySelector("#omnitrix")
 const omnitrixBtn = document.querySelector("#omnitrix .green-btn")
 const omnitrixDisplay = document.querySelector("#omnitrix .mid-portion .frame")
 const lights = document.querySelector("#omnitrix .lights")
+const root = document.querySelector(":root")
+
+// Text Elements
 const alienName = document.querySelector("#alienName")
+const playlist = document.querySelector("#playlist")
 
-let currentPlaylist = 0
-let currentAlienId = 0
+// JS Variables
+let timeout = 0
 
+// Event Listeners
 omnitrixBtn.addEventListener("click", () => {
     if (omnitrixDisplay.classList.contains("active")) {
         toggleOmnitrixDisplay()
         playAudio("../sounds/beep.ogg")
     }
+    if (!omnitrixDisplay.classList.contains("up")) {
+        hideAlienName()
+    }
 })
 
 omnitrixBtn.addEventListener("contextmenu", (e) => {
     e.preventDefault()
+
     changePlaylist()
+    if (omnitrixDisplay.classList.contains("up")) showAlienName()
+    showPlaylist()
+
     playAudio("../sounds/beep.ogg")
 })
 
 omnitrixDisplay.addEventListener("click", () => {
-    if (isOmnitrixDisplayUp()) {
-        // Transform
-        toggleOmnitrixDisplay()
-        playAudio("./sounds/transformation_1.ogg")
-        omnitrixDisplay.classList.remove("active")
-    } else if (
-        !omnitrixDisplay.classList.contains("active") &&
-        !omnitrixDisplay.classList.contains("inactive")
-    ) {
-        // Detransform and deactivate
-        playAudio("./sounds/end.ogg")
-        omnitrixTimeOut()
-        alienName.innerText = ""
-    } else if (omnitrixDisplay.classList.contains("inactive")) {
-        // Reactivate
-        playAudio("./sounds/initiate.ogg")
-        omnitrixDisplay.classList.remove("inactive")
-        omnitrixDisplay.classList.add("active")
+    if (!omnitrixDisplay.classList.contains("processing")) {
+        if (isOmnitrixDisplayUp()) {
+            // Transform
+            toggleOmnitrixDisplay()
+            playAudio("./sounds/transformation_1.ogg")
+            omnitrixDisplay.classList.remove("active")
+
+            transformOmnitrix()
+        } else if (!omnitrixDisplay.classList.contains("active") && !omnitrixDisplay.classList.contains("inactive")) {
+            // Detransform and deactivate
+            playAudio("./sounds/end.ogg")
+            omnitrixTimeOut()
+            hideAlienName()
+        } else if (omnitrixDisplay.classList.contains("inactive")) {
+            // Reactivate
+            playAudio("./sounds/initiate.ogg")
+            omnitrixDisplay.classList.remove("inactive")
+            omnitrixDisplay.classList.add("active")
+        }
     }
 })
 
 omnitrixDisplay.addEventListener("contextmenu", (e) => {
     e.preventDefault()
-    // if (omnitrixDisplay.classList.contains("up") && omnitrixDisplay.style.animation == "") {
     if (
         omnitrixDisplay.classList.contains("up") &&
-        !(
-            omnitrixDisplay.classList.contains("rotate-right") ||
-            omnitrixDisplay.classList.contains("rotate-left")
-        )
+        !(omnitrixDisplay.classList.contains("rotate-right") || omnitrixDisplay.classList.contains("rotate-left"))
     ) {
         let isRight = e.clientX >= window.innerWidth / 2
         isRight ? turnOmnitrix("right") : turnOmnitrix("left")
     }
 })
+
+// Functions
 
 const isOmnitrixDisplayUp = () => {
     if (omnitrixDisplay.classList.contains("up")) {
@@ -88,13 +100,14 @@ const turnOmnitrix = (direction) => {
     omnitrixDisplay.classList.add(rotation)
     setTimeout(() => {
         omnitrixDisplay.classList.remove(rotation)
-    }, 1000)
+    }, 500)
 }
 
 const omnitrixTimeOut = () => {
     let isTimingOut = true
 
     omnitrixDisplay.classList.toggle("inactive")
+    omnitrixDisplay.classList.add("processing")
 
     setInterval(() => {
         if (!isTimingOut) {
@@ -106,7 +119,27 @@ const omnitrixTimeOut = () => {
     setTimeout(() => {
         isTimingOut = false
         omnitrixDisplay.classList.add("inactive")
+        omnitrixDisplay.classList.remove("processing")
+        transformOmnitrix()
     }, 3000)
+}
+
+const transformOmnitrix = () => {
+    if (!omnitrixDisplay.classList.contains("inactive")) {
+        root.style.setProperty("--og-frame", 'url("../images/frame.png")')
+        root.style.setProperty("--base", "none")
+        omnitrixBtn.style.display = "none"
+        changeBackColor(true)
+    } else {
+        root.style.setProperty("--og-frame", 'url("../images/new-frame.png")')
+        root.style.setProperty("--base", 'url("../images/base.png")')
+        omnitrixBtn.style.display = "block"
+        changeBackColor(false)
+    }
+}
+
+const changeBackColor = (isTransforming = true) => {
+    root.style.setProperty("--background-color", isTransforming == true ? alienColorsMatrix[currentPlaylist][currentAlienId] : "blueviolet")
 }
 
 const playAudio = (audioFile) => {
@@ -115,16 +148,8 @@ const playAudio = (audioFile) => {
 }
 
 const randomTurnAudio = () => {
-    let audio = new Audio(
-        Math.random() > 0.5
-            ? "./sounds/left_turn.ogg"
-            : "./sounds/right_turn.ogg"
-    )
+    let audio = new Audio(Math.random() > 0.5 ? "./sounds/left_turn.ogg" : "./sounds/right_turn.ogg")
     audio.play()
-}
-
-const showAlienName = () => {
-    alienName.innerText = playlistMatrix[currentPlaylist][currentAlienId]
 }
 
 const changePlaylist = () => {
@@ -151,4 +176,43 @@ const changeAlien = (direction) => {
     }
 
     showAlienName()
+    storeAlienAndPlaylistInLocalStorage()
 }
+
+const showPlaylist = () => {
+    playlist.innerText = "Playlist " + (currentPlaylist + 1)
+
+    clearTimeout(timeout)
+
+    timeout = setTimeout(() => {
+        playlist.innerText = ""
+    }, 2000)
+}
+
+const showAlienName = () => {
+    alienName.innerText = playlistMatrix[currentPlaylist][currentAlienId]
+}
+
+const hideAlienName = () => {
+    alienName.innerText = ""
+}
+
+const storeAlienAndPlaylistInLocalStorage = () => {
+    localStorage.setItem("alienId", currentAlienId)
+    localStorage.setItem("playlist", currentPlaylist)
+}
+
+// Gets
+
+const getPlaylist = () => {
+    const playlist = localStorage.getItem("playlist")
+    return playlist !== null ? playlist : 0
+}
+
+const getAlienID = () => {
+    const alienID = localStorage.getItem("alienId")
+    return alienID !== null ? alienID : 0
+}
+
+let currentPlaylist = getPlaylist()
+let currentAlienId = getAlienID()
